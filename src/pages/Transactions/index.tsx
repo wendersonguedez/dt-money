4353be846ba988ad7c33085bbc513ddcc1c57939
wenderson <wendersonguedes6@gmail.com>
2025-12-03 17:49:01 -0300

    feat: criado componente de busca de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 
 import {
 	PriceHighlight,
@@ -14,6 +15,7 @@ export function Transactions() {
 			<Summary />
 
 			<TransactionsContainer>
+				<SearchForm />
 				<TransactionsTable>
 					<tbody>
 						<tr>
