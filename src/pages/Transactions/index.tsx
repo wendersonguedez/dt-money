edbb8edb35af24f7e7e6fe4466244af71c259f7a
wenderson <wendersonguedes6@gmail.com>
2025-12-03 04:30:10 -0300

    feat: criado componente de tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 
+import {
+	PriceHighlight,
+	TransactionsContainer,
+	TransactionsTable,
+} from "./styles";
+
 export function Transactions() {
 	return (
 		<div>
 			<Header />
 			<Summary />
+
+			<TransactionsContainer>
+				<TransactionsTable>
+					<tbody>
+						<tr>
+							<td width="50%">Desenvolvimento de site</td>
+							<td>
+								<PriceHighlight variant="income">R$ 50.000,00</PriceHighlight>
+							</td>
+							<td>Venda</td>
+							<td>14/08/2023</td>
+						</tr>
+						<tr>
+							<td width="50%">Podrão</td>
+							<td>
+								<PriceHighlight variant="outcome">- R$ 50,00</PriceHighlight>
+							</td>
+							<td>ALimentação</td>
+							<td>14/08/2023</td>
+						</tr>
+					</tbody>
+				</TransactionsTable>
+			</TransactionsContainer>
 		</div>
 	);
 }
